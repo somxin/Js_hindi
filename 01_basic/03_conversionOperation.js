19b0c516f847c945e65b0e2abd2b35d5e8151c7c
somxin <somxyz625@gmail.com>
2025-12-05 19:39:01 +0000

    conversion of dataTypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,48 @@
+/*
+let score = 33 
+
+console.log(typeof score);
+console.log(typeof(score));
+*/
+
+
+/*
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+*/
+
+/*
+//let score = "33abc"      ->valueInNumber contain NaN
+//let score = null         ->valueInNumber contain NaN
+//let score = undefined    ->valueInNumber contain NaN
+
+console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);    // It will print value of valueInNumber
+*/
+
+/*
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0=> false
+// "" => false
+// "hitesh" => true
+*/
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
