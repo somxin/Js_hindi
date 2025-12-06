5f698df4cb97c14a870185cda69d194ecc10a607
somxin <somxyz625@gmail.com>
2025-12-06 05:44:08 +0000

    Operations

---
+++ 01_basic/03_conversionOperation2.js
@@ -0,0 +1,42 @@
+//*******Operations*******
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3) // 2 power 3
+//console.log(2%3)  // remainder
+
+let str1 = "Hello"
+let str2 = " hitesh"
+
+let str3 = str1+str2
+console.log(str3)
+
+
+// console.log("1"+2) // 12
+// console.log(1+"2") // 12
+// console.log("1"+2+2) // 122
+// console.log(1+2+"2") // 32
+
+
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+// let x = 3
+// const y = x++
+// console.log(x,y)
+
+let x = 3
+const y = ++x
+console.log(x,y)
+
+
