1528645c2b1f99ae1084bb15298e81575fc6ed3f
somxin <somxyz625@gmail.com>
2025-12-06 14:59:17 +0000

    Comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2 != 1)
+
+//Note:- Always compare same datatype
+/*
+console.log("2">1)
+console.log("02">1)
+*/
+/*
+Avoid this
+console.log(null > 0) // false
+console.log(null == 0) // false
+console.log(null >= 0) //true 
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+*/
+
+
+// strict check ===
+console.log("2"===2)
+
