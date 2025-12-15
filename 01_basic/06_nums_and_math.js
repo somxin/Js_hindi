2a4272c10866b289ffa88fbb1a8ddd2082203a53
somxin <somxyz625@gmail.com>
2025-12-15 14:54:19 +0000

    math and nums

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,52 @@
+/*
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+//Methods 
+console.log(balance.toString())
+console.log(balance.toFixed(2))
+
+const otherNumber = 1123.8966
+console.log(otherNumber.toPrecision(4)) // Very Important
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString())  //1,000,000
+*/
+
+/// +++++++++++++++++++++ Maths +++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+console.log(Math.sqrt(25));
+console.log(Math.pow(7,2));
+
+console.log(Math.min(4,3,6,8));
+console.log(Math.max(4,3,6,8));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+//Use of random
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
+
+
+
+
+
+
+
+
+
+
